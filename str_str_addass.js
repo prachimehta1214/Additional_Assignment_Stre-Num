b1adbdd1621ee906c20619a73945a9d034943e76
prachimehta1214 <42134655+prachimehta1214@users.noreply.github.com>
2018-08-10 11:02:30 +0200

    Update str_str_addass.js

--- str_str_addass.js
+++ str_str_addass.js
@@ -1,1 +1,11 @@
+//str_str
 
+let str_1 = "1";
+let str_str;
+{ // str_1 += "1";
+  str_1 = str_1 + "1";
+  str_str = str_1; 
+};
+let actual = "1";
+actual += "1";
+console.assert(str_str === actual, "fail: str_str");
